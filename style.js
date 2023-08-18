32e12a2cb3633928932b35c157fa3a9dcfbce2f1
thenghia <nghiadang085@gmail.com>
2023-08-18 10:45:03 +0700

    Edit hover image

--- style.js
+++ style.js
@@ -1,13 +1,29 @@
-const list = document.querySelecterAll('span')
-var index = 0
+// function showTime(){
+//     var date = new Date();
+//     var h = date.getHours(); // 0 - 23
+//     var m = date.getMinutes(); // 0 - 59
+//     var s = date.getSeconds(); // 0 - 59
+//     var session = "AM";
+    
+//     if(h == 0){
+//         h = 12;
+//     }
+    
+//     if(h > 12){
+//         h = h - 12;
+//         session = "PM";
+//     }
+    
+//     h = (h < 10) ? "0" + h : h;
+//     m = (m < 10) ? "0" + m : m;
+//     s = (s < 10) ? "0" + s : s;
+    
+//     var time = h + ":" + m + ":" + s + " " + session;
+//     document.getElementById("MyClockDisplay").innerText = time;
+//     document.getElementById("MyClockDisplay").textContent = time;
+    
+//     setTimeout(showTime, 1000);
+    
+// }
 
-setInterval((e) => {
-    list.forEach((e) => {
-        e.classList.remove('change-properties')
-    })
-    list[index].classList.add('change-properties')
-    index++ 
-    if( index ==list.length){
-        index = 0
-    }
-},200)
+// showTime();
